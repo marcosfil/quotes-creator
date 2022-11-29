eaa797802cf2b6578b9c4e6e4672dd517c6c37ca
marcosfil <marcosfilho222@gmail.com>
2022-11-29 16:02:26 +0100

    first commit

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Max2", text: "Learning React is bad!" },
+  { id: "q3", author: "Max3", text: "Learning React is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
