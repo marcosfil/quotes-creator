90df8f2b7f4231cc1117eaf82823e7cdcc0245c4
marcosfil <marcosfilho222@gmail.com>
2022-11-30 23:55:41 +0100

    second commit

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,31 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Max2", text: "Learning React is bad!" },
+  { id: "q3", author: "Max3", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return (
+      <Fragment>
+        <HighlightedQuote text={"No Quote Found!"}></HighlightedQuote>
+      </Fragment>
+    );
+  }
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
